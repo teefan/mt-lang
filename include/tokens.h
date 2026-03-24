4cf5819988d2f11f80679ba8cb4a3b19eb848332
teefan <teefan82@gmail.com>
2026-03-24 14:22:35 +0700

    Update token type naming definition

--- include/tokens.h
+++ include/tokens.h
@@ -3,14 +3,14 @@
 // Ký hiệu của ngôn ngữ lập trình Milk Tea
 typedef enum TokenType
 {
-    // --- Ký tự đơn (Single-character tokens) ---
+    // --- Ký tự đơn ---
     TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,     // ( )
     TOKEN_LEFT_BRACKET, TOKEN_RIGHT_BRACKET, // [ ]
     TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,     // { }
     TOKEN_COLON, TOKEN_SEMICOLON,            // : ;
     TOKEN_COMMA, TOKEN_DOT,                  // , .
 
-    // --- Toán tử & Ký tự kép (One or two character tokens) ---
+    // --- Toán tử & Ký tự kép ---
     TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,          // = ==
     TOKEN_BANG, TOKEN_BANG_EQUAL,            // ! !=
     TOKEN_LESS, TOKEN_LESS_EQUAL,            // < <=
@@ -20,11 +20,11 @@ typedef enum TokenType
     TOKEN_STAR, TOKEN_SLASH,                 // * /
     TOKEN_PERCENT,                           // % (Chia lấy dư)
 
-    // --- Các toán tử đặc trưng của Milk Tea ---
-    TOKEN_DOT_DOT,                           // .. (Range)
-    TOKEN_ARROW,                             // -> (Return type)
-    TOKEN_FAT_ARROW,                         // => (Lambda/Body)
-    TOKEN_SLASH_SLASH,                       // // (Integer division)
+    // --- Các toán tử đặc trưng ---
+    TOKEN_DOT_DOT,                           // .. Khoảng
+    TOKEN_ARROW,                             // -> Kiểu trả về
+    TOKEN_FAT_ARROW,                         // => Nội dung hàm ẩn danh
+    TOKEN_SLASH_SLASH,                       // //
 
     // --- Giá trị văn bản ---
     TOKEN_IDENTIFIER,
@@ -32,9 +32,9 @@ typedef enum TokenType
     TOKEN_STRING_BLOCK,
     TOKEN_NUMBER,
     TOKEN_CHAR,
-    TOKEN_TRUE, TOKEN_FALSE,                 // bool literals
+    TOKEN_TRUE, TOKEN_FALSE,                 // giá trị đúng/sai
 
-    // --- Quản lý và an toàn bộ nhớ ---
+    // --- Từ khóa: Quản lý và an toàn bộ nhớ ---
     TOKEN_OWN, TOKEN_REF, TOKEN_MANY, TOKEN_RAW,
     TOKEN_NULL,
     TOKEN_STACK, TOKEN_HEAP,
@@ -49,7 +49,7 @@ typedef enum TokenType
     TOKEN_FUNCTION,
     TOKEN_LET, TOKEN_FIXED, TOKEN_CONST,
 
-    // --- Từ khóa: Điều khiển luồng (Control Flow) ---
+    // --- Từ khóa: Điều khiển luồng ---
     TOKEN_IF, TOKEN_ELSE,
     TOKEN_SWITCH, TOKEN_CASE,
     TOKEN_FOR, TOKEN_IN, TOKEN_WHILE,
@@ -58,7 +58,7 @@ typedef enum TokenType
     TOKEN_AS,                               // Ép kiểu: x as T
     TOKEN_IS,                               // Kiểm tra kiểu: x is T
 
-    // --- Kí hiệu đặc biệt để phân tích từ ngữ (Layout-based) ---
+    // --- Kí hiệu đặc biệt để phân tích từ ngữ (kiểu thụt lề) ---
     TOKEN_NEWLINE,
     TOKEN_INDENT,
     TOKEN_DEDENT,
