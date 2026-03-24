5e155b0360aea88b0b17237385549a84dab5b590
teefan <teefan82@gmail.com>
2026-03-24 14:33:08 +0700

    Define Token strucure

--- src/lexer.c
+++ src/lexer.c
@@ -1,7 +1,16 @@
 #include "tokens.h"
 #include <stdio.h>
+#include <stdint.h>
 
-void init_lexer()
+#define MAX_INDENT_LEVEL 64 // Cấp độ thụt lề tối đa
+
+typedef struct Token
 {
-    printf("Lexer initialized!\n");
-}
+    TokenType type; // Kiểu ký hiệu
+
+    uint32_t offset; // Vị trí byte tương đối trong mã nguồn
+    uint16_t length; // Độ dài của ký hiệu
+
+    uint16_t leading_trivia_length; // Độ dài khoảng trắng và ghi chú đi trước ký hiệu
+    uint16_t trailing_trivia_length; // Độ dài khoảng trắng và ghi chú đi sau ký hiệu
+} Token;
