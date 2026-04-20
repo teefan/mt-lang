46965ffc61e6cd378906916a36286005147935f5
teefan <teefan82@gmail.com>
2026-04-20 13:17:53 +0700

    Init project

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+ruby "4.0.1"
+
+gem "rake", "~> 13.4"
+
+group :development, :test do
+  gem "minitest", "~> 6.0"
+end
---
+++ lib/milk_tea.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'milk_tea/token'
+require_relative 'milk_tea/lexer'
+require_relative 'milk_tea/ast'
+require_relative 'milk_tea/parser'
+require_relative 'milk_tea/module_loader'
+require_relative 'milk_tea/bindgen'
+require_relative 'milk_tea/sema'
+require_relative 'milk_tea/types'
+require_relative 'milk_tea/ir'
+require_relative 'milk_tea/lowering'
+require_relative 'milk_tea/codegen'
+require_relative 'milk_tea/c_backend'
+require_relative 'milk_tea/cli'
+
+module MilkTea
+  def self.root
+    @root ||= Pathname.new(File.expand_path('..', __dir__))
+  end
+end
