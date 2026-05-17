80eee11f6ca682b9934ff0234695671289cc44fc
teefan <teefan82@gmail.com>
2026-05-17 14:03:55 +0700

    Add quickfix actions for redundant inline unsafe and return statements

--- lib/milk_tea/lsp/server.rb
+++ lib/milk_tea/lsp/server.rb
@@ -1110,40 +1110,80 @@ module MilkTea
 
           when 'redundant-unsafe'
             next if source_line.empty?
-            next unless source_line.match?(/\A\s*unsafe:\s*\z/)
+            if source_line.match?(/\A\s*unsafe:\s*\z/)
+              lines = content.lines
+              unsafe_idx = diag_line - 1
+              first_body_idx = unsafe_idx + 1
+              next if first_body_idx >= lines.length
 
-            lines = content.lines
-            unsafe_idx = diag_line - 1
-            first_body_idx = unsafe_idx + 1
-            next if first_body_idx >= lines.length
+              unsafe_indent = lines[unsafe_idx].match(/\A(\s*)/)[1]
+              body_indent = unsafe_indent + '    '
 
-            unsafe_indent = lines[unsafe_idx].match(/\A(\s*)/)[1]
-            body_indent = unsafe_indent + '    '
-
-            body_end_idx = first_body_idx - 1
-            (first_body_idx...lines.length).each do |i|
-              line = lines[i]
-              if line.chomp.empty? || line.start_with?(body_indent)
-                body_end_idx = i
-              else
-                break
+              body_end_idx = first_body_idx - 1
+              (first_body_idx...lines.length).each do |i|
+                line = lines[i]
+                if line.chomp.empty? || line.start_with?(body_indent)
+                  body_end_idx = i
+                else
+                  break
+                end
               end
-            end
-            next if body_end_idx < first_body_idx
+              next if body_end_idx < first_body_idx
 
-            new_body = lines[first_body_idx..body_end_idx].map { |line| line.sub(/\A    /, '') }.join
+              new_body = lines[first_body_idx..body_end_idx].map { |line| line.sub(/\A    /, '') }.join
+              actions << {
+                title: 'Remove redundant unsafe',
+                kind: 'quickFix',
+                diagnostics: [diag],
+                edit: {
+                  changes: {
+                    uri => [{
+                      range: {
+                        start: { line: unsafe_idx,       character: 0 },
+                        end:   { line: body_end_idx + 1, character: 0 }
+                      },
+                      newText: new_body
+                    }]
+                  }
+                }
+              }
+            else
+              new_line = Linter.remove_inline_unsafe_prefix(source_line, column: diag_start_char + 1)
+              next if new_line == source_line
+
+              actions << {
+                title: 'Remove redundant unsafe',
+                kind: 'quickFix',
+                diagnostics: [diag],
+                edit: {
+                  changes: {
+                    uri => [{
+                      range: {
+                        start: { line: diag_line - 1, character: 0 },
+                        end:   { line: diag_line,     character: 0 }
+                      },
+                      newText: new_line
+                    }]
+                  }
+                }
+              }
+              end
+
+          when 'redundant-return'
+            next unless source_line.match?(/\A\s*return\s*\z/)
+
             actions << {
-              title: 'Remove redundant unsafe',
+              title: 'Remove redundant return',
               kind: 'quickFix',
               diagnostics: [diag],
               edit: {
                 changes: {
                   uri => [{
                     range: {
-                      start: { line: unsafe_idx,       character: 0 },
-                      end:   { line: body_end_idx + 1, character: 0 }
+                      start: { line: diag_line - 1, character: 0 },
+                      end:   { line: diag_line,     character: 0 }
                     },
-                    newText: new_body
+                    newText: ''
                   }]
                 }
               }
