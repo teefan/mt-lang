1e49820e6598cc68d2cfac8e6426980488486843
teefan <teefan82@gmail.com>
2026-05-24 22:09:24 +0700

    Fix indentation in snake_case method for consistent formatting

--- lib/milk_tea/bindings/imported_bindings.rb
+++ lib/milk_tea/bindings/imported_bindings.rb
@@ -1299,7 +1299,7 @@ module MilkTea
 
       def snake_case(name)
         name.to_s
-            .gsub(/([A-Z]+)([A-Z][a-z])/, '\\1_\\2')
+          .gsub(/([A-Z]+)([A-Z][a-z])/, '\\1_\\2')
           .gsub(/([a-z0-9])([A-Z])/, '\\1_\\2')
           .downcase
           .gsub(/([a-z])(\d+)_d\b/, '\\1_\\2d')
