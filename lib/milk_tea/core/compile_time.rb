686d4630e4e5bbb2fc57e6b60dc827a463bdb1d1
teefan <teefan82@gmail.com>
2026-06-10 23:09:33 +0700

    Fix compile-time function lookup to access top_level_functions correctly

--- lib/milk_tea/core/compile_time.rb
+++ lib/milk_tea/core/compile_time.rb
@@ -72,7 +72,7 @@ module MilkTea
             },
             resolve_call: ->(call_expr) {
               if call_expr.callee.is_a?(AST::Identifier)
-                func = @checker.top_level_function(call_expr.callee.name)
+                func = @checker.send(:top_level_function, call_expr.callee.name)
                 if func&.ast&.respond_to?(:const) && func.ast.const
                   begin
                     initial_vars = {}
