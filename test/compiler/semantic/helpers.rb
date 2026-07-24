f0c90e6c6ae59bc041f79c1e7655115c29e15c74
teefan <teefan82@gmail.com>
2026-07-24 18:59:46 +0700

    test: add duplicate suppression and cross-file immutable-this tests
    - test_cross_file_immutable_this_not_duplicated: verifies exactly 1 immutable-this
      error (not duplicated by validate_specialized_function_body)
    - test_cross_file_type_mismatch_and_immutable_this_both_reported: verifies type
      mismatch still surfaces while immutable-this is counted once
    - Added check_program_source_collecting helper that aggregates import errors +
      collecting_path_errors from all checked modules

--- test/compiler/semantic/helpers.rb
+++ test/compiler/semantic/helpers.rb
@@ -28,4 +28,38 @@ module SemaTestHelpers
       MilkTea::ModuleLoader.new(module_roots: [dir, MilkTea.root]).check_program(root_path)
     end
   end
+
+  def check_source_collecting(source)
+    Dir.mktmpdir("milk-tea-sema") do |dir|
+      root_path = File.join(dir, source_relative_path(source))
+      FileUtils.mkdir_p(File.dirname(root_path))
+      File.write(root_path, source)
+
+      loader = MilkTea::ModuleLoader.new(module_roots: [dir, MilkTea.root])
+      ast = loader.load_file(root_path)
+      import_result = loader.send(:imported_modules_for_ast_collecting_errors, ast, importer_path: root_path)
+      result = MilkTea::SemanticAnalyzer.check_collecting_errors(ast, imported_modules: import_result.modules, path: root_path)
+      result[:errors]
+    end
+  end
+
+  def check_program_source_collecting(source, imported_sources = {})
+    Dir.mktmpdir("milk-tea-sema") do |dir|
+      root_path = File.join(dir, source_relative_path(source))
+      FileUtils.mkdir_p(File.dirname(root_path))
+      File.write(root_path, source)
+
+      imported_sources.each do |relative_path, imported_source|
+        path = File.join(dir, relative_path)
+        FileUtils.mkdir_p(File.dirname(path))
+        File.write(path, imported_source)
+      end
+
+      loader = MilkTea::ModuleLoader.new(module_roots: [dir, MilkTea.root])
+      ast = loader.load_file(root_path)
+      import_result = loader.send(:imported_modules_for_ast_collecting_errors, ast, importer_path: root_path)
+      result = MilkTea::SemanticAnalyzer.check_collecting_errors(ast, imported_modules: import_result.modules, path: root_path)
+      import_result.errors + result[:errors] + loader.collecting_path_errors.values.flatten
+    end
+  end
 end
